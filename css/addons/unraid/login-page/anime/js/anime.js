0fd4da2fbe288abd00755d6e4b88982b96525081
Olivier <59826338+Olivier-Vromans@users.noreply.github.com>
2023-07-25 00:23:50 +0200

    Update anime.js

--- css/addons/unraid/login-page/anime/js/anime.js
+++ css/addons/unraid/login-page/anime/js/anime.js
@@ -1,6 +1,6 @@
 // Adds a video background
 
 let video = `
-<div class="background-wrap"><video id="video-bg-elem" preload="auto" autoplay="true" loop="loop" muted="muted"> <source src="https:\/\/olivier-vromans.github.io\/theme.park\/css\/addons\/unraid\/login-page\/anime\/video\/anime.mp4">Video not supported</video></div>
+<div class="background-wrap"><video id="video-bg-elem" preload="auto" autoplay loop muted playsinline> <source src="https:\/\/olivier-vromans.github.io\/theme.park\/css\/addons\/unraid\/login-page\/anime\/video\/anime.mp4">Video not supported</video></div>
 `
 document.body.innerHTML += video
